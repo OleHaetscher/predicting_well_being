fdcb53da0d326c3f7bdaafc035881f3f454cf2db
OleHaetscher <olehaetscher@web.de>
2024-11-16 12:30:40 +0100

    add some more diagnostics for the shap ia values computations

--- src/analysis/RFRAnalyzer.py
+++ src/analysis/RFRAnalyzer.py
@@ -121,21 +121,22 @@ class RFRAnalyzer(BaseMLAnalyzer):
         for idx, x in X_subset.iterrows():
             ia_values_obj = explainer.explain(x.values)
             ia_value_lst.append(ia_values_obj)
+        self.logger.log(f"Currently processing these indices: {X_subset.index[:3]} -> COMPLETED")
 
         ## remove later -> simple test for correct feature assignment
         # check first and last feature for first row
-        for test_idx in [1,2,3]:
+        #for test_idx in [1,2,3]:
             # Extract the string index from X_subset for the given test index
-            sample_index = X_subset.index[test_idx]
+            #sample_index = X_subset.index[test_idx]
 
             # Extract the features at columns 0 and 88 for the row corresponding to test_idx
-            feature_name_0 = X_subset.columns[0]  # Column name for index 0
+            #feature_name_0 = X_subset.columns[0]  # Column name for index 0
             #feature_name_88 = X_subset.columns[88]  # Column name for index 88
 
 
-            test_val_1 = ia_value_lst[test_idx].dict_values[(0,)]
+            #test_val_1 = ia_value_lst[test_idx].dict_values[(0,)]
             #test_val_2 = ia_value_lst[test_idx].dict_values[(88,)]
-            print(f"shap_ia_val for {feature_name_0} and sample {sample_index}: {test_val_1}")
+            #print(f"shap_ia_val for {feature_name_0} and sample {sample_index}: {test_val_1}")
             #print(f"shap_ia_val for {feature_name_88} and sample {sample_index}: {test_val_2}")
 
         return ia_value_lst
